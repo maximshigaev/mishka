3668ead08109907fadfaf208b09a46036834e5a2
Maxim <financial_monster@mail.ru>
2022-05-16 10:05:47 +0400

    add accessebility for the Intro and TopOffer components

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -8,8 +8,22 @@ export const Intro: FC = () => (
     <section className="intro">
       <h1 className="intro__title">Милые штуки ручной <br /> работы для дома</h1>
       <ul className="intro__assortment-list">
-        <li className="intro__assortment-item intro__assortment-item--interior">Предметы интерьера</li>
-        <li className="intro__assortment-item intro__assortment-item--toys">Детские игрушки</li>
+        <li className="intro__assortment-item intro__assortment-item--interior">
+          <a className="intro__assortment-link intro__assortment-link--interior"
+            href=""
+            title="Предметы интерьера"
+          >
+            Предметы интерьера
+          </a>
+        </li>
+        <li className="intro__assortment-item intro__assortment-item--toys">
+          <a className="intro__assortment-link intro__assortment-link--toys"
+            href=""
+            title="Детские игрушки"
+          >
+            Детские игрушки
+          </a>
+        </li>
       </ul>
     </section>
   </div>
